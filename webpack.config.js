5a1ee56a5adc783eef7278b4ffc36f23b19e22bc
Elmer <s0227373691@gmail.com>
2020-08-13 21:48:35 +0800

    新增打包圖片設定

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,16 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: { limit: 40000 },
+          },
+          "image-webpack-loader",
+        ],
+      },
     ],
   },
 };
