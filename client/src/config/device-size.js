db63ea3984de9de9a50cacee005d102c98792d08
Elmer <s0227373691@gms.tku.edu.tw>
2020-08-17 11:33:35 +0800

    新增JSdoc

--- client/src/config/device-size.js
+++ client/src/config/device-size.js
@@ -1,3 +1,13 @@
+/** @constant {object}
+    @desc 各螢幕裝置尺寸
+    @var mobileS: 320px
+    @var mobileM: 375px
+    @var mobileL: 425px
+    @var tablet: 768px
+    @var laptop: 1024px
+    @var laptopL: 1440px
+    @var desktop: 2560px
+*/
 const device = {
   mobileS: "(max-width: 320px)",
   mobileM: "(max-width: 375px)",
