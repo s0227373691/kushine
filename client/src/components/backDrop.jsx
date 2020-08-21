7127c7eaf8c1196afa8ae48e170927b625dde822
Elmer <s0227373691@gms.tku.edu.tw>
2020-08-22 01:09:34 +0800

    create component backDrop.jsx

---
+++ client/src/components/backDrop.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const BackDrop = ({ show, clicked }) =>
+  show ? <BackDropDisplay onClick={clicked} /> : null;
+
+export default BackDrop;
+
+const BackDropDisplay = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  z-index: 100;
+  left: 0;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
